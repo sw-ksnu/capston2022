f8b8757a72f77cffcb6b022f10d2d22c9f9574a8
Sunglahihi <94941481+Sunglahihi@users.noreply.github.com>
2022-05-03 11:35:46 +0900

    change print phrase

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
-    
-    System.out.println("hihihihihihihi");
+    System.out.println("Hello Seong Min");
   }
 }
